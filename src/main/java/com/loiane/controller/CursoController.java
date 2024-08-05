88dd39d3c20b02c0daf6ee339b522879bb168624
Walmir Wild <walmirww@gmail.com>
2024-08-05 08:00:17 -0300

    HTTP Post

--- src/main/java/com/loiane/controller/CursoController.java
+++ src/main/java/com/loiane/controller/CursoController.java
@@ -3,6 +3,7 @@ package com.loiane.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.loiane.model.Course;
@@ -10,7 +11,12 @@ import com.loiane.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -25,6 +31,19 @@ public class CursoController {
     public List<Course> list() {
         return courseRepository.findAll();
     }       
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);        
+    }
+    /*public ResponseEntity<Course> create(@RequestBody Course course) {
+        //System.out.println(course.getName());
+        //return courseRepository.save(course);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(courseRepository.save(course));
+    }*/
+    
     
     
 }
