f48ffe9df0078066398283b3dddb0faf61bcb31b
Walmir Wild <walmirww@gmail.com>
2024-08-06 10:19:21 -0300

    HTTP Delete

--- src/main/java/com/loiane/controller/CursoController.java
+++ src/main/java/com/loiane/controller/CursoController.java
@@ -13,11 +13,11 @@ import lombok.AllArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
@@ -58,6 +58,16 @@ public class CursoController {
                 recordFound.setCategory(course.getCategory());
                 Course updated = courseRepository.save(recordFound);
                 return ResponseEntity.ok().body(updated);
+            })
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        return courseRepository.findById(id)
+            .map(recordFound -> {
+                courseRepository.deleteById(id);
+                return ResponseEntity.noContent().<Void>build();
             })
             .orElse(ResponseEntity.notFound().build());
     }
