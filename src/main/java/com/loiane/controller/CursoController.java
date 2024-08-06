057d7df5cae3ad3d21693922e97a71b39d1ad764
Walmir Wild <walmirww@gmail.com>
2024-08-06 09:23:00 -0300

    HTTP Put

--- src/main/java/com/loiane/controller/CursoController.java
+++ src/main/java/com/loiane/controller/CursoController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -38,7 +40,7 @@ public class CursoController {
     @GetMapping("/{id}")
     public ResponseEntity<Course> findById(@PathVariable long id) {
         return courseRepository.findById(id)
-            .map(record -> ResponseEntity.ok().body(record))
+            .map(recordFound -> ResponseEntity.ok().body(recordFound))
             .orElse(ResponseEntity.notFound().build());
     }
 
@@ -47,6 +49,19 @@ public class CursoController {
     public Course create(@RequestBody Course course) {
         return courseRepository.save(course);        
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable long id, @RequestBody Course course) {
+        return courseRepository.findById(id)
+            .map(recordFound -> {
+                recordFound.setName(course.getName());
+                recordFound.setCategory(course.getCategory());
+                Course updated = courseRepository.save(recordFound);
+                return ResponseEntity.ok().body(updated);
+            })
+            .orElse(ResponseEntity.notFound().build());
+    }
+    
     /*public ResponseEntity<Course> create(@RequestBody Course course) {
         //System.out.println(course.getName());
         //return courseRepository.save(course);
