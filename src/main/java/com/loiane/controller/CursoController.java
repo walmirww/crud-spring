c939980a331cd53b67a956b19684b1b7ad21641d
Walmir Wild <walmirww@gmail.com>
2024-08-06 05:49:50 -0300

    Get by ID

--- src/main/java/com/loiane/controller/CursoController.java
+++ src/main/java/com/loiane/controller/CursoController.java
@@ -14,8 +14,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -30,7 +33,14 @@ public class CursoController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
-    }       
+    }   
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable long id) {
+        return courseRepository.findById(id)
+            .map(record -> ResponseEntity.ok().body(record))
+            .orElse(ResponseEntity.notFound().build());
+    }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
