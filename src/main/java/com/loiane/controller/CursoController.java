7b6f30b5f074457854f52c66b6c9eae9d1b179da
Walmir Wild <walmirww@gmail.com>
2024-08-07 07:29:50 -0300

    API with Java Beans Validation

--- src/main/java/com/loiane/controller/CursoController.java
+++ src/main/java/com/loiane/controller/CursoController.java
@@ -9,10 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.loiane.model.Course;
 import com.loiane.repository.CourseRepository;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,11 +24,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
-
-
-
 @RestController
+@Validated
 @RequestMapping("/api/courses/")
 @AllArgsConstructor
 public class CursoController {
@@ -38,7 +39,7 @@ public class CursoController {
     }   
     
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable long id) {
+    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive long id) {
         return courseRepository.findById(id)
             .map(recordFound -> ResponseEntity.ok().body(recordFound))
             .orElse(ResponseEntity.notFound().build());
@@ -46,12 +47,12 @@ public class CursoController {
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody Course course) {
+    public Course create(@RequestBody @Valid Course course) {
         return courseRepository.save(course);        
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> update(@PathVariable long id, @RequestBody Course course) {
+    public ResponseEntity<Course> update(@PathVariable @NotNull @Positive long id, @RequestBody @Valid Course course) {
         return courseRepository.findById(id)
             .map(recordFound -> {
                 recordFound.setName(course.getName());
@@ -63,7 +64,7 @@ public class CursoController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable long id) {
+    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive long id) {
         return courseRepository.findById(id)
             .map(recordFound -> {
                 courseRepository.deleteById(id);
--- src/main/java/com/loiane/model/Course.java
+++ src/main/java/com/loiane/model/Course.java
@@ -1,5 +1,7 @@
 package com.loiane.model;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -7,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -18,10 +23,16 @@ public class Course {
     @JsonProperty("_id")
     private long id;
 
-    @Column(length = 200, nullable = false)
+    @NotNull
+    @NotBlank
+    @Length(min = 5, max = 100)
+    @Column(length = 100, nullable = false)
     private String name;
 
-    @Column(length = 200, nullable = false)
+    @NotNull
+    @Length(max = 10)
+    @Column(length = 10, nullable = false)
+    @Pattern(regexp = "front-end|back-end")
     private String category;
     
 }
