c5ed056186809807d056545fa22fcd621cc80f43
Walmir Wachholz Wild <39489944+walmirww@users.noreply.github.com>
2024-08-20 06:15:04 -0300

    Validations (API + Enums)

--- src/main/java/com/loiane/dto/CourseDTO.java
+++ src/main/java/com/loiane/dto/CourseDTO.java
@@ -5,14 +5,17 @@ import java.util.List;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.loiane.enums.Category;
+import com.loiane.enums.validation.ValueOfEnum;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record CourseDTO(
-        @JsonProperty("_id") Long id, 
-        @NotNull @NotBlank @Length(min = 5, max = 100) String name, 
-        @NotBlank @Length(max = 10) @Pattern(regexp = "front-end|back-end") String category,
-        List<LessonDTO> lessons) {    
-    }
+        @JsonProperty("_id") Long id,
+        @NotBlank @NotNull @Length(min = 5, max = 100) String name,
+        @NotNull @Length(max = 10) @ValueOfEnum(enumClass = Category.class) String category,
+        @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
+}
